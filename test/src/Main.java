caea151a7e6393d13aa58b42cbf6e70032c8345e
susu144178 <susu144178@gmail.com>
2022-09-22 13:11:57 +0900

    java test

---
+++ test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // ** 주석 : 컴퓨터가 인식하지 못하는 영역
+
+        /*
+        * 주석 : 여러줄의 영역을 인식하지 못하게 해야할 경우에 사용
+         */
+        System.out.println("Hello world!");
+        System.out.println("김선우");
+
+    }
+}
