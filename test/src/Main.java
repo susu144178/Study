a3bbb4d77c28203aa53e4569f82f5f803b7f9cf5
susu144178 <susu144178@gmail.com>
2022-09-23 13:26:21 +0900

    step_01

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,14 +1,56 @@
 public class Main {
     public static void main(String[] args) {
         // ** 주석 : 컴퓨터가 인식하지 못하는 영역
+        // ** 변수 : 값이 언제든지 변경 가능한 수.
+        // ** 상수 : 값의 변경이 불가능한 수.
+
+        // ** lvalue = rvalue
+
+        // ** 변수 선언
+        int number;
+
+        // ** 변수 초기화
+        number = 10;
+
+        // ** 변수 선언 및 초기화
+        int num = 100;
+
+        // ** 출력
+        System.out.println(number);
+        System.out.println(num);
+
+        // ** 값 변경
+        number = 20;
+        num = 200;
+
+        // ** 출력
+        System.out.println(number);
+        System.out.println(num);
+
+        // =================================================
+        // [자 료 형]
+        // =================================================
+
 
         /*
-        * 주석 : 여러줄의 영역을 인식하지 못하게 해야할 경우에 사용
+        * [정수형 datatype]
+
+        문자를 표현할때 사용
+        * char : 2 byte
+
+        숫자
+        * byte : 1 byte
+        * short : 2 byte
+        * int : 4 byte
+        * long : 8 byte
+
+        * [실수형 datatype]
+        * float : 4 byte
+        * double : 8 byte
+
+        * [논리형 datatype]
+        * boolean : 1 byte
          */
-        System.out.println("Hello world!");
-        System.out.println("김선우");
-        System.out.println("임꺽정");
-        System.out.println("두두찡");
 
     }
 }
